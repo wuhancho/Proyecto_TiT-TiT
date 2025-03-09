50e9a1e4089c92afce06a34d93a19073cef67edc
wuhancho <juanmigueljimenez2014@gmail.com>
2025-03-09 16:20:36 +0100

    parte dos de las pruebas de la linterna

--- Assets/Script/Linterna.cs
+++ Assets/Script/Linterna.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class Linterna : MonoBehaviour
 {
+    [SerializeField] Material Transparent;
+    [SerializeField] MeshRenderer meshRender;
     private void OnTriggerEnter(Collider other)
     {
-        if (CompareTag("ViewLint"))
+        print("entra en el trigger de la linterna");
+        if (other.CompareTag("ViewLint"))
         {
-            
+            //meshRender = other.GetComponent<MeshRenderer>();
+            print("entra en el if de la linterna");
+            var materials = other.GetComponent<MeshRenderer>().materials;
+            var color = materials[1].color;
+            color.a = 1f;
+            materials[1].color = color;
+            //if (other.GetComponent<MeshRenderer>().materials[1].name=="base 2"/*meshRender.materials[1].name=="base 2" || meshRender.materials[0].name == "base 2"*/)
+            //{
+            //    print("entra en el if de la linterna 2");
+            //}
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ViewLint"))
+        {
+            var materials = other.GetComponent<MeshRenderer>().materials;
+            var color = materials[1].color;
+            color.a = 0;
+            materials[1].color = color;
         }
     }
 }
