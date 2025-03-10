a2b78fbd25ba4cac36a057965f0b09e9eaf7c7b2
wuhancho <151606064+wuhancho@users.noreply.github.com>
2025-03-10 17:18:36 +0100

    pruebas de la linterna

--- Assets/Script/Linterna.cs
+++ Assets/Script/Linterna.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class Linterna : MonoBehaviour
 {
-    [SerializeField] Material Transparent;
-    [SerializeField] MeshRenderer meshRender;
+    private Material[] materials;
+    private Color color;
     private void OnTriggerEnter(Collider other)
     {
         print("entra en el trigger de la linterna");
+
         if (other.CompareTag("ViewLint"))
         {
             //meshRender = other.GetComponent<MeshRenderer>();
             print("entra en el if de la linterna");
-            var materials = other.GetComponent<MeshRenderer>().materials;
-            var color = materials[1].color;
-            color.a = 1f;
-            materials[1].color = color;
+            if (other.GetComponent<MeshRenderer>())
+            {
+                materials = other.GetComponent<MeshRenderer>().materials;
+                if (materials[1].name == "base 2 (Instance)")
+                {
+                    print("entra en el if de la linterna 2");
+                    color = materials[1].color;
+
+                }
+            }
+            else if (other.gameObject.name == "L_")
+            {
+                
+            }
             //if (other.GetComponent<MeshRenderer>().materials[1].name=="base 2"/*meshRender.materials[1].name=="base 2" || meshRender.materials[0].name == "base 2"*/)
             //{
             //    print("entra en el if de la linterna 2");
@@ -27,8 +38,8 @@ public class Linterna : MonoBehaviour
     {
         if (other.CompareTag("ViewLint"))
         {
-            var materials = other.GetComponent<MeshRenderer>().materials;
-            var color = materials[1].color;
+            materials = other.GetComponent<MeshRenderer>().materials;
+            color = materials[1].color;
             color.a = 0;
             materials[1].color = color;
         }
