97c91b52365d618ae6c842c7fa3435154622fcac
JORGE IGNACIO PEREZ FLOREZ <moabencod3@gmail.com>
2025-06-02 15:43:20 +0200

    Sonidos arreglados

--- Assets/sonidos/ReproducirAudio.cs
+++ Assets/sonidos/ReproducirAudio.cs
@@ -1,27 +1,65 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class ReproducirAudio : MonoBehaviour
+//{
+
+//    AudioSource fuenteAudio;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        fuenteAudio = GetComponent<AudioSource>();
+//    }
+
+//    // Update is called once per frame
+//    void OnTriggerEnter()
+//    {
+//        gameObject.GetComponent<Collider>().enabled = false;
+//        fuenteAudio.Play();
+//    }
+//    void OnTriggerExit()
+//    {
+//        gameObject.GetComponent<Collider>().enabled = true;
+
+//    }
+//}
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ReproducirAudio : MonoBehaviour
 {
-
     AudioSource fuenteAudio;
 
-    // Start is called before the first frame update
     void Start()
     {
         fuenteAudio = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void OnTriggerEnter()
+    void OnTriggerEnter(Collider other)
     {
-        gameObject.GetComponent<Collider>().enabled = false;
-        fuenteAudio.Play();
+        if (!AudioManager.audioReproduciendose)
+        {
+            StartCoroutine(ReproducirUnaVez());
+        }
     }
-    void OnTriggerExit()
+
+    IEnumerator ReproducirUnaVez()
     {
-        gameObject.GetComponent<Collider>().enabled = true;
-        
+        AudioManager.audioReproduciendose = true;
+
+        // Desactivar collider para evitar reentradas
+        GetComponent<Collider>().enabled = false;
+
+        fuenteAudio.Play();
+
+        yield return new WaitForSeconds(fuenteAudio.clip.length);
+
+        AudioManager.audioReproduciendose = false;
+
+        // Reactivar el collider después si lo necesitas
+        GetComponent<Collider>().enabled = true;
     }
 }
